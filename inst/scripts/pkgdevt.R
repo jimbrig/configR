4b7b383170ffb8c10b5f05cae283e663bb7be022
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-05-14 21:02:43 -0400

    feat: update pkgdevt script

--- inst/scripts/pkgdevt.R
+++ inst/scripts/pkgdevt.R
@@ -7,7 +7,21 @@
 #
 #  ------------------------------------------------------------------------
 
-# library R packages ------------------------------------------------------
+# require / library dev packages ------------------------------------------
+c(
+  "usethis",
+  "devtools",
+  "pkgbuild",
+  "pkgload",
+  "pkgdown",
+  "testthat",
+  "knitr",
+  "rmarkdown",
+  "chameleon",
+  "attachment",
+  "desc"
+) |>
+  sapply(require, character.only = TRUE)
 
 library(devtools)
 library(usethis)
@@ -29,9 +43,9 @@ usethis::create_package("configR")
 usethis::use_namespace()
 usethis::use_roxygen_md()
 usethis::use_git()
-usethis::use_tibble() # #' @return a [tibble][tibble::tibble-package]
-usethis::use_pipe()
-usethis::use_tidy_eval()
+# usethis::use_tibble() # #' @return a [tibble][tibble::tibble-package]
+# usethis::use_pipe()
+# usethis::use_tidy_eval()
 devtools::document()
 
 
@@ -39,14 +53,14 @@ devtools::document()
 
 # set description and title first so included in GH repo
 desc::desc_set(
-  "Description" = "My awesome description.",
-  "Title" = "My awesome title"
+  "Description" = "Setup and configure an optimal R development environment.",
+  "Title" = "R Conviguration Package"
 )
 
 usethis::use_github(private = FALSE)
 
 # github labels -----------------------------------------------------------
-
+library(templateeR)
 templateeR::use_gh_labels()
 
 
@@ -63,7 +77,10 @@ usethis::use_news_md()
 c(
   # add function file names here:
   "utils",
-  "zzz"
+  "zzz",
+  "r_config_dir",
+  "rver",
+  "install_dev_packages"
 ) |> purrr::walk(usethis::use_r, open = FALSE)
 
 
@@ -71,7 +88,8 @@ c(
 
 c(
   # add function test file names here:
-
+  "r_config_dir",
+  "rver"
 ) |> purrr::walk(usethis::use_test)
 
 # data --------------------------------------------------------------------
@@ -88,6 +106,53 @@ c(
   "configR"
 
 ) |> purrr::walk(usethis::use_vignette)
+
+
+
+# meta --------------------------------------------------------------------
+
+require(codemetar)
+codemetar::write_codemeta()
+codemetar::give_opinions('C:\Users\jimmy\Dev\jimbrig\configR')
+
+# templates ---------------------------------------------------------------
+
+c(
+  ".config/R/config",
+  ".config/R/templates",
+  ".config/R/lib",
+  ".config/R/cache",
+  ".config/R/dev",
+  ".config/R/profile",
+  ".config/R/rstudio",
+  ".config/R/radian",
+  ".config/R/bin",
+  ".config/R/profile/options"
+) |>
+  purrr::map_chr(~ fs::path("inst/templates/", .x)) |>
+  purrr::walk(fs::dir_create)
+
+c(
+  ".Rprofile",
+  ".Renviron",
+  ".Rbuildignore",
+  ".Rhistory",
+  "config.yml",
+  "README.md",
+  "bin/init.R",
+  "bin/rstudio.R",
+  "bin/packages.R",
+  "bin/vscode.R",
+  "bin/accounts.R",
+  "bin/installs.R",
+  "bin/configure.R",
+  "profile/profile_functions.R",
+  "profile/profile_options.R",
+  "profile/profile_secrets.R",
+  "profile/profile_settings.R",
+  "profile/profile_shortcuts.R"
+) |>
+  purrr::walk(~ fs::file_create(fs::path("inst/templates/.config/R", .x)))
 
 
 
